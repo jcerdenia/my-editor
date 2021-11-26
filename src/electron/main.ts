16c654089fa895f083818bca3db894b0b3f4d3a9
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-11-26 20:43:36 +0800

    Enable persisting data through filesystem

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,4 +1,21 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const contentFile = path.join(app.getPath('userData'), 'content.html');
+
+ipcMain.handle('getContent', () => {
+  if (fs.existsSync(contentFile)) {
+    const result = fs.readFileSync(contentFile);
+    return result.toString();
+  }
+
+  return '';
+});
+
+ipcMain.handle('setContent', ({}, content: string) => {
+  fs.writeFileSync(contentFile, content);
+});
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
